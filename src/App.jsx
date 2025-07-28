cfd9845961e3c6d06b35caf1a8be57464788d8e2
RISHITHA <rishitha22k@gmail.com>
2025-07-28 10:26:12 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - Rishitha</h1>
+      <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
